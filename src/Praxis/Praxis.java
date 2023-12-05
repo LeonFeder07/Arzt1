dcb88128432cd62e252002b7be108a317d4b42db
leon.feder <leonfeder18@gmail.com>
2023-12-05 13:08:56 +0100

    da

--- src/Praxis/Praxis.java
+++ src/Praxis/Praxis.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 public class Praxis {
     Queue <Patient> blutschlange;
     Queue <Patient> arztschlange;
+    Stack <Patient> rechnung;
     Patient platz1;
     private JPanel artzpraxis;
     private JTextField textField1;
@@ -23,16 +24,20 @@ public class Praxis {
     private JTextField textField4;
     private JLabel label5;
     private JTextField textField5;
+    private JButton stackbuttom;
+    private JTextField textField6;
 
     public Praxis() {
 blutschlange= new Queue <Patient>();
         arztschlange= new Queue <Patient>();
+        rechnung= new Stack<Patient>();
 
         aufnehmenAButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Patient pPatient=new Patient(textField1.getText(),textField2.getText());
                 arztschlange.enqueue(pPatient);
+
             textField1.setText("");
             textField2.setText("");
                 Patient lokal = arztschlange.front();
@@ -44,14 +49,26 @@ blutschlange= new Queue <Patient>();
         aufrufenAButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Patient lokal = arztschlange.front();
+              Patient  lokal = arztschlange.front();
+             if(arztschlange.front()!=null) {
+                 rechnung.push(arztschlange.front());
 
-                textField4.setText(lokal.getNachname()+", "+ lokal.getVorname());
-                textField3.setText("");
-                arztschlange.dequeue();
-                textField1.setText("");
-                textField2.setText("");
+                 textField6.setText(rechnung.top().getNachname() +","+ rechnung.top().getVorname());
+             }
+                if(arztschlange.front()!=null) {
 
+
+                    textField4.setText(lokal.getNachname() + ", " + lokal.getVorname());
+                    textField3.setText("");
+                    arztschlange.dequeue();
+                    lokal=arztschlange.front();
+                    textField1.setText("");
+                    textField2.setText("");
+
+                    if(arztschlange.front()!=null) {
+                        textField3.setText(lokal.getNachname() + ", " + lokal.getVorname());
+                    }
+                }
 
             }
         });
@@ -60,6 +77,7 @@ blutschlange= new Queue <Patient>();
             public void actionPerformed(ActionEvent e) {
                 Patient pPatient=new Patient(textField1.getText(),textField2.getText());
                 blutschlange.enqueue(pPatient);
+
                 textField1.setText("");
                 textField2.setText("");
                 Patient lokal = blutschlange.front();
@@ -70,11 +88,34 @@ blutschlange= new Queue <Patient>();
         aufrufenBlutdruckButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Patient lokal = blutschlange.front();
+                Patient  lokal = blutschlange.front();
+                if(arztschlange.front()!=null) {
+                    rechnung.push(blutschlange.front());
+                    textField6.setText(rechnung.top().getNachname() +","+ rechnung.top().getVorname());
+                }
+               if(blutschlange.front()!=null) {
 
-                textField4.setText(lokal.getNachname()+", "+ lokal.getVorname());
-                blutschlange.dequeue();
-                textField5.setText("");
+
+                   textField4.setText(lokal.getNachname() + ", " + lokal.getVorname());
+                   blutschlange.dequeue();
+                   lokal = blutschlange.front();
+                   textField5.setText("");
+
+                   if(blutschlange.front()!=null) {
+                   textField5.setText(lokal.getNachname() + ", " + lokal.getVorname());
+               }}
+            }
+        });
+        stackbuttom.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                    rechnung.pop();
+                if(!rechnung.isEmpty()) {
+                    textField6.setText(rechnung.top().getNachname() + "," + rechnung.top().getVorname());
+                }else{
+                    textField6.setText("");
+                }
             }
         });
     }
